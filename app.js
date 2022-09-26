f3b2eb3d462729236431330f7da0c44612e3e6ea
Arnaud BODEL <contact@osw3.net>
2022-09-26 13:56:23 +0200

    add index file

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 "use strict";
 
+const addition = require('./index');
+
 console.log("Hello There !");
 console.log("Seconde ligne de log");
+
+console.log( addition(10, 5) );
---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+exports.modules = function (a, b)
+{
+    return a + b;
+}
